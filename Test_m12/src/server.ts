690613852b0c9e44730e2ba925da91a3c6a5f365
Hridoy-ikramul-haque <67628732+Hridoy-ikramul-haque@users.noreply.github.com>
2026-01-21 04:23:20 +0600

    users crud done

--- Test_m12/src/server.ts
+++ Test_m12/src/server.ts
@@ -47,10 +47,56 @@ initDB();
 // parser 
 app.use(express.json());
 
-
+// users crud 
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello World!!");
 })
+
+
+app.get('/users', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`Select * from users`);
+        res.status(200).json({
+            success: true,
+            message: "user retrieved successfully",
+            data: result.rows
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.meassage
+        })
+    }
+})
+
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+    // console.log(req.params.id);
+    // res.send("check");
+    try {
+        const result = await pool.query(`SELECT * FROM users WHERE id=$1`, [req.params.id]);
+        // console.log(result.rows);
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "Sorry......."
+            })
+        }
+        else {
+            res.status(200).json({
+                success: true,
+                message: "User Fetched successfully",
+                data: result.rows[0]
+            })
+        }
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+})
+
 
 app.post('/users', async (req: Request, res: Response) => {
     // console.log(req.body);
@@ -78,21 +124,67 @@ app.post('/users', async (req: Request, res: Response) => {
     // })
 })
 
-app.get('/users', async (req: Request, res: Response) => {
+
+
+app.put('/users/:id', async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+
     try {
-        const result = await pool.query(`Select * from users`);
-        res.status(200).json({
-            success: true,
-            message: "user retrieved successfully",
-            data: result.rows
-        })
+        const result = await pool.query(`UPDATE users SET name=$1,email=$2 WHERE id=$3 RETURNING *`, [name, email, req.params.id])
+
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "data update failed"
+            })
+        }
+        else {
+            res.status(200).json({
+                success: true,
+                message: "data updated",
+                data: result.rows[0]
+
+            })
+        }
     } catch (error: any) {
         res.status(500).json({
-            success: false,
-            message: error.meassage
+            success: "false",
+            error: error.message
         })
     }
 })
+
+
+app.delete('/users/:id', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`DELETE FROM users WHERE id=$1`, [req.params.id]);
+        if (result.rowCount === 0) {
+            res.status(404).json({
+                success: false,
+                message: "there no such id......."
+            })
+        }
+        else {
+            res.status(200).json({
+                success: true,
+                message: `ID NUMBER ${req.params.id} data deleted`,
+                data: result.rows
+            })
+        }
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+})
+
+
+
+
+// todos crud 
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
